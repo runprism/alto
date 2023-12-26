f13e2662548a12dafe25a73d18767eeba3a634df
Mihir Trivedi <mihirtrivedi@mihirs-mbp.lan>
2023-12-26 14:53:25 -0500

    small bug in regular ec2 script...download files from container

--- nomad/agents/scripts/docker/ec2/run.sh
+++ nomad/agents/scripts/docker/ec2/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts p:u:n:a:z:r:i: flag
+while getopts p:u:n:a:z:r:i:f:d: flag
 do
 	case "${flag}" in
 		p) pem_path=${OPTARG};;
@@ -10,6 +10,8 @@ do
 		z) password=${OPTARG};;
 		r) repository=${OPTARG};;
 		i) image_name=${OPTARG};;
+		f) download_files=${OPTARG};;
+		d) project_dir=${OPTARG};;
 	esac
 done
 
@@ -30,19 +32,27 @@ container_id=$(ssh -i ${pem_path} ${user}@${public_dns_name} "docker run -d ${re
 
 # Check if container_id is not empty
 if [ -n "$container_id" ]; then
-    echo "Container started successfully. ID: $container_id"
-    echo "Streaming logs from container $container_id"
-    
 	# Run docker logs for the container
-    ssh -i ${pem_path} ${user}@${public_dns_name} "docker logs $container_id"
+    ssh -i ${pem_path} ${user}@${public_dns_name} "docker logs -f $container_id"
 else
     echo "Failed to start the container."
 fi
 
-# # Now, download the files
-# for val in "${download_files[@]}"; do
-# 	scp -i ${pem_path} ${user}@${public_dns_name}:../../${project_dir}/${val} ${project_dir}/${val} 2> scp.log
-# 	if [ $exit_code -eq 1 ]; then
-# 		exit 1
-# 	fi
-# done
+# Now, download the files
+project_name="$(basename -- ${project_dir})"
+for val in "${download_files[@]}"; do
+	# Copy the file from the container to the instances
+	ssh -i ${pem_path} ${user}@${public_dns_name} "docker cp $container_id:./${project_name}/${val} ${val}"
+	exit_code=$?
+	if [ $exit_code -eq 1 ]; then
+		exit 1
+	fi
+
+	# Copy the file from the instance to local
+	echo "Copying ${val} to local machine..."
+	scp -i ${pem_path} ${user}@${public_dns_name}:${val} ${project_dir}/${val} 2> scp.log
+	exit_code=$?
+	if [ $exit_code -eq 1 ]; then
+		exit 1
+	fi
+done
